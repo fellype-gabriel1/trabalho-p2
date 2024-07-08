51e4b19bb094ef31b2bcf2cadc56c77a08894c20
Fellype Gabriel <166552474+fellype-gabriel1@users.noreply.github.com>
2024-07-08 18:53:24 -0300

    Add files via upload

---
+++ fatorial.py
@@ -0,0 +1,5 @@
+n1 = int(input("Diite um numero: "))
+fatorial = 1
+for n in range (1,n1+1):
+    fatorial *= n
+    print(fatorial)
---
+++ soma dos valores de 1 a 100.py
@@ -0,0 +1,5 @@
+#soma dos valores de 1 a 100
+soma = 0
+for numero in range(1,101):
+    soma += numero
+    print(soma)
